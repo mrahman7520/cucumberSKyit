8c78bab842ae60531daaf09cf1bd75e48708b7a3
mrahman7520 <134352546+mrahman7520@users.noreply.github.com>
2023-06-03 22:39:45 -0500

    Update Hooks.java

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -9,10 +9,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Hooks extends Base{
 	@Before
 	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver(); 
-		driver.manage().window().maximize();
-		System.out.println("before run");
+// 		WebDriverManager.chromedriver().setup();
+// 		driver = new ChromeDriver(); 
+// 		driver.manage().window().maximize();
+// 		System.out.println("before run");
+		
+ChromeOptions opt = new ChromeOptions();
+		
+	 driver = new RemoteWebDriver(new URL("http://3.145.197.131:4444"),opt);
+		
 	}
 	
 	@After

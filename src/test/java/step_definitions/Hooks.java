5b3d783cf0f905294464603327389866cde3018f
mrahman7520 <134352546+mrahman7520@users.noreply.github.com>
2023-06-03 22:46:26 -0500

    Update Hooks.java

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -2,19 +2,24 @@ package step_definitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
 import base.Base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hooks extends Base{
 	@Before
-	public void setup() {
+	public void setup() throws MalformedURLException {
 // 		WebDriverManager.chromedriver().setup();
 // 		driver = new ChromeDriver(); 
 // 		driver.manage().window().maximize();
 // 		System.out.println("before run");
 		
-ChromeOptions opt = new ChromeOptions();
+         ChromeOptions opt = new ChromeOptions();
 		
 	 driver = new RemoteWebDriver(new URL("http://3.145.197.131:4444"),opt);
 		
